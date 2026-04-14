a383e76bf4ff8530e353ed9b086a2d57afe5cf0b
BouramaSang <sangarebourama245@gmail.com>
2026-04-14 10:37:54 +0000

    separation du sidebar

---
+++ src/README.txt
@@ -0,0 +1,32 @@
+INAPP INVENTORY DASHBOARD
+
+INSTALLATION :
+
+1. Ouvrir le dossier dans VS Code
+
+2. Ouvrir le terminal
+
+3. Exécuter la commande :
+
+   npx serve
+
+4. Ouvrir dans le navigateur :
+   http://localhost:3000
+
+IMPORTANT :
+
+* Ne pas ouvrir index.html directement
+* Une connexion internet est requise (CDN Bootstrap et ApexCharts)
+
+STRUCTURE :
+
+* index.html → page principale
+* assets/css → styles
+* assets/js → scripts
+* assets/images → images
+
+PERSONNALISATION :
+
+* Modifier index.html pour changer l’interface
+* Modifier style.css pour le design
+* Modifier chart.js pour les graphiques
---
+++ src/sidebar.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="./assets/images/favicon_io/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon_io/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon_io/favicon-16x16.png">
+  <link rel="manifest" href="./assets/images/favicon_io/site.webmanifest">
+    <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Tabler Icons -->
+  <link href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+ <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <!-- SIDEBAR -->
+  <aside id="sidebar" class="sidebar">
+    <div class="logo-area">
+     <a href="index.html" class="d-inline-flex"><img src="./assets/images/logo-icon.svg" alt="" width="24">
+        <span class="logo-text ms-2"> <img src="./assets/images/logo.svg" alt=""></span>
+      </a>
+    </div>
+    <ul class="nav flex-column">
+      <li class="px-4 py-2"><small class="nav-text">Main</small></li>
+      <li><a class="nav-link active" href="index.php"><i class="ti ti-home"></i><span
+            class="nav-text">Dashboard</span></a></li>
+             <li class="px-4 py-2"><small class="nav-text">Enregistrements</small></li>
+      <li><a class="nav-link" href="category.php"><i class="ti ti-category"></i><span
+            class="nav-text"> Add Category</span></a></li>
+      <li><a class="nav-link" href="create-product.php"><i class="ti ti-package"></i><span class="nav-text">Add
+            Product</span></a></li>
+    <li><a class="nav-link" href="reports.php"><i class="ti ti-receipt"></i><span class="nav-text">Reports</span></a>
+      </li>
+    <li><a class="nav-link" href="404-error.php"><i class="ti ti-alert-circle"></i><span class="nav-text">404 Error</span></a>
+      </li>
+      <li><a class="nav-link" href="docs.php"><i class="ti ti-file-text"></i><span class="nav-text">Docs</span></a></li>
+
+      <li class="px-4 pt-4 pb-2"><small class="nav-text">Account</small></li>
+      <li><a class="nav-link" href="signin.php"><i class="ti ti-logout"></i><span class="nav-text">Log in</span></a>
+      </li>
+      <li><a class="nav-link" href="signup.php"><i class="ti ti-user-plus"></i><span class="nav-text">Sign
+            up</span></a></li>
+    </ul>
+
+    
+  </aside>
+</body>
+</html>
